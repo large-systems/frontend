7b0492844d30418c1b6a84488e73fdd0cf44c573
cphjs <cph-js408@cphbusiness.dk>
2019-11-17 05:20:54 -0800

    Setup testing framework; implement example blackbox integration test

---
+++ FrontendTests/Integration/Booking/IndexPageText.cs
@@ -0,0 +1,28 @@
+﻿using AngleSharp;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FrontendTests.Integration.Booking
+{
+    public class IndexPageText : IntegrationTest
+    {
+        public IndexPageText(HotelApplicationFactory factory) : base(factory) { }
+
+        [Fact]
+        public async Task Get_Index_ReturnsPageWithSearchFormAndCreateButton()
+        {
+            var response = await _factory.CreateClient().GetAsync("/Booking");
+            var content = await response.Content.ReadAsStringAsync();
+            var doc = await BrowsingContext.New().OpenAsync(req => req.Content(content));
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.NotNull(doc.QuerySelector("form"));
+            var createBtn = doc.QuerySelector("form a");
+            Assert.NotNull(createBtn);
+            Assert.Equal("Create New", createBtn.TextContent.ToString());
+        }
+
+    }
+}
---
+++ FrontendTests/Integration/HotelApplicationFactory.cs
@@ -0,0 +1,14 @@
+﻿using HotelSystem_Frontend;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace FrontendTests.Integration
+{
+    public class HotelApplicationFactory : WebApplicationFactory<Startup>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+
+        }
+    }
+}
