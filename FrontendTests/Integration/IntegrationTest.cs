cca7bf2319ea6d81000909ecbcc940ac37d896f1
cphjs <47212256+cphjs@users.noreply.github.com>
2019-11-17 15:32:25 +0100

    Setup testing framework; create example test; setup azure pipeline to run tests and publish results (#3)
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Try to target nestandard
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Setup testing framework; implement example blackbox integration test
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Remove dependencies that this project should have never had
    
    * Cleanup

---
+++ FrontendTests/Integration/IntegrationTest.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace FrontendTests.Integration
+{
+    public class IntegrationTest : IClassFixture<HotelApplicationFactory>
+    {
+        protected readonly HotelApplicationFactory _factory;
+
+        public IntegrationTest(HotelApplicationFactory factory)
+        {
+            _factory = factory;
+        }
+    }
+}
